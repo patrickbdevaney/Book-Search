1e0d8c4fc9ac9d750c862324841f404b35104367
patrickbdevaney <79435941+patrickbdevaney@users.noreply.github.com>
2021-08-26 20:28:36 -0400

    fix

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -44,7 +44,7 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const response = await addUser(userFormData);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
