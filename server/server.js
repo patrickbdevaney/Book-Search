bed82a3bc4dd00de3a66366ea9f423ec19890618
patrickbdevaney <79435941+patrickbdevaney@users.noreply.github.com>
2021-08-26 20:55:20 -0400

    adding server.js imports

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const mongoose = require("mongoose")
 
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
